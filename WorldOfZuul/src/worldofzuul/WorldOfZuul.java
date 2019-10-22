724582248ab22f320fe8e68c8fe513b09b556260
Marcus <salted1337@protonmail.com>
2019-10-22 12:11:56 +0200

    Final fucking fix

---
+++ WorldOfZuul/src/worldofzuul/WorldOfZuul.java
@@ -0,0 +1,12 @@
+package worldofzuul;
+
+public class WorldOfZuul {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
