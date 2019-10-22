a381c183c1821f21e936195035fd26f79789e683
Marcus <salted1337@protonmail.com>
2019-10-22 12:19:30 +0200

    Ja goddag, vi man spille spillet

--- WorldOfZuul/src/worldofzuul/WorldOfZuul.java
+++ WorldOfZuul/src/worldofzuul/WorldOfZuul.java
@@ -2,11 +2,9 @@ package worldofzuul;
 
 public class WorldOfZuul {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Game game = new Game();
+        game.play();
     }
     
 }
