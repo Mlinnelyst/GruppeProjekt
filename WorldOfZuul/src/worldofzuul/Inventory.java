9db05b6732ccd1c575a590d93ff48815126e4623
Mads <mlinnelyst11@gmail.com>
2019-10-29 10:51:14 +0100

    Tilføjet PrintInventory()

--- WorldOfZuul/src/worldofzuul/Inventory.java
+++ WorldOfZuul/src/worldofzuul/Inventory.java
@@ -3,4 +3,15 @@ import java.util.ArrayList;
 
 public class Inventory {
     public ArrayList<Trash> trash;
+
+    // Print alle navne på skrald i spillerens inventory
+    public void PrintInventory() {
+        System.out.println("-----Inventory-----");
+        
+        trash.forEach((trash1) -> {
+            System.out.println(trash1.toString());
+        });
+        
+        System.out.println("-------------------");
+    }
 }
