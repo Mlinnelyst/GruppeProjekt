46909069fbb85f5c72a697c903da7664777acee8
Mads <mlinnelyst11@gmail.com>
2019-11-19 10:31:03 +0100

    Oprettet gui project

---
+++ WorldOfZuulGui/WorldOfZuulGui/src/main/java/com/mycompany/worldofzuulgui/main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.worldofzuulgui;
+
+/**
+ *
+ * @author mads
+ */
+public class main {
+    public static void main(String[] args) {
+        App.load(args);
+    }
+}
