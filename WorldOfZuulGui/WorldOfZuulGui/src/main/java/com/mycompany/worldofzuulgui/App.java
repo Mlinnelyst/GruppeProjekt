e07e97170577ce630c1369ee638be223c47d1496
Mads <mlinnelyst11@gmail.com>
2019-11-19 13:09:06 +0100

    Suicide

--- WorldOfZuulGui/WorldOfZuulGui/src/main/java/com/mycompany/worldofzuulgui/App.java
+++ WorldOfZuulGui/WorldOfZuulGui/src/main/java/com/mycompany/worldofzuulgui/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
     
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Hjem"), 640, 480);
+        scene = new Scene(loadFXML("Hjem"), 960, 720);
         stage.setScene(scene);
         stage.show();
     }
