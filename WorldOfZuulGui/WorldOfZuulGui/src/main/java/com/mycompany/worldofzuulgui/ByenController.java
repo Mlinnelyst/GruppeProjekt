997db1365ac1effe2db0453177f34673afcb1259
Marcus <salted1337@protonmail.com>
2019-12-03 16:29:48 +0100

    Billeder til skrald. Kun noget af det

--- WorldOfZuulGui/WorldOfZuulGui/src/main/java/com/mycompany/worldofzuulgui/ByenController.java
+++ WorldOfZuulGui/WorldOfZuulGui/src/main/java/com/mycompany/worldofzuulgui/ByenController.java
@@ -33,18 +33,18 @@ public class ByenController implements Initializable {
     private Button goHjem;
     @FXML
     private Pane spawnPane;
-    
+
     public Button[] arrayButtons;
-    
+
     public ImageView[] arrayImage;
-    
+
     @FXML
     private Button btn1;
     @FXML
     private Button btn2;
     @FXML
     private Button btn3;
-    
+
     ArrayList<Trash> trashList = new ArrayList<Trash>();
     @FXML
     private Pane inventoryPane;
@@ -56,61 +56,70 @@ public class ByenController implements Initializable {
     private ImageView f3;
     @FXML
     private ImageView background;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         App.game.inventory.reloadInv(inv1, inv2);
-        
+
         trashList.add(new Trash("bananskrald", TrashType.BANANA));
         trashList.add(new Trash("juice", TrashType.JUICE));
         trashList.add(new Trash("apple", TrashType.APPLE));
         trashList.add(new Trash("avocado", TrashType.AVOCADO));
         trashList.add(new Trash("beerframe", TrashType.BEERFRAME));
-        
-        arrayButtons = new Button[] { btn1, btn2, btn3 };
-        arrayImage = new ImageView[] {f1,f2,f3};
-        
-        
+
+        arrayButtons = new Button[]{btn1, btn2, btn3};
+        arrayImage = new ImageView[]{f1, f2, f3};
+
         int bX = 795;
         int bY = 597;
+
+        for (Trash trash : trashList) {
+            System.out.println("Trash: " + trash.toString());
+        }
         
-        for (int i = 0; i < arrayImage.length; i++){
+        for (int i = 0; i < arrayImage.length; i++) {
             int rnd = new Random().nextInt(trashList.size());
             
             arrayImage[i].setId(trashList.get(rnd).getTrashType().toString());
-            arrayImage[i].se
-
+            
+            File currentImage = new File("file:///" + System.getProperty("user.dir") + 
+                        "\\src\\main\\java\\com\\mycompany\\JavaBilleder1\\" + trashList.get(rnd).getTrashType().toString() + ".png");
+            
+            String currentImageDir = currentImage.getPath();            
+            //System.out.println("Image: " + currentImage.getPath() + ". Exists: " + currentImage.exists());
+         
+            arrayImage[i].setImage(new Image(currentImage.getPath()));
+            
             // Random position
             arrayImage[i].setLayoutX(new Random().nextInt(bX));
             arrayImage[i].setLayoutY(new Random().nextInt(bY));
         }
-        
-        
-    }    
+
+    }
 
     @FXML
-    private void goHjemAction(ActionEvent event)throws IOException {
+    private void goHjemAction(ActionEvent event) throws IOException {
         App.game.play(new Command(CommandWord.GO, "hjem", "", ""));
         App.setRoot("Hjem");
     }
 
     @FXML
     private void trashClicked(ActionEvent event) {
-        
+
     }
-    
+
     @FXML
     private Button inv1;
-    
+
     @FXML
     private Button inv2;
-    
+
     @FXML
     private void invClicked1(ActionEvent event) {
         App.game.inventory.btn1Clicked(event);
     }
-    
+
     @FXML
     private void invClicked2(ActionEvent event) {
         App.game.inventory.btn2Clicked(event);
@@ -118,8 +127,8 @@ public class ByenController implements Initializable {
 
     @FXML
     private void trashClicked(MouseEvent event) {
-       App.game.inventory.trashClicked(event, trashList, inv1, inv2);
+        App.game.inventory.trashClicked(event, trashList, inv1, inv2);
         System.out.println("ada");
     }
-    
+
 }
