fcc367019a90d1e83a30a500e9fcafb46a157f90
Marcus <salted1337@protonmail.com>
2019-12-03 16:29:48 +0100

    Billeder til skrald. Kun noget af det

--- WorldOfZuulGui/WorldOfZuulGui/src/main/java/com/mycompany/worldofzuulgui/main.java
+++ WorldOfZuulGui/WorldOfZuulGui/src/main/java/com/mycompany/worldofzuulgui/main.java
@@ -5,12 +5,24 @@
  */
 package com.mycompany.worldofzuulgui;
 
+import java.io.File;
+
 /**
  *
  * @author mads
  */
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {      
+        
+        String currentDir = System.getProperty("user.dir") + "\\src\\main\\java\\com\\mycompany\\JavaBilleder1";        
+        File dir = new File(currentDir);
+        
+        for (File file : dir.listFiles()) {
+            System.out.println("File: " + file.getName() + ". IsDirectory: " + file.isDirectory());
+        }
+        
+        //System.out.println("Current dir: " + dir);
+        
         App.load(args);
     }
 }
