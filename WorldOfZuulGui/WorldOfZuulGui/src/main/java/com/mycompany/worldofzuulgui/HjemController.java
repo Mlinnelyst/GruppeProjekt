be7b774003d666aa73a700e49c5f190694693c59
Mads98 <madsmunch1998@gmail.com>
2019-12-09 11:16:39 +0100

    Spwan i hjem op nye billeder

--- WorldOfZuulGui/WorldOfZuulGui/src/main/java/com/mycompany/worldofzuulgui/HjemController.java
+++ WorldOfZuulGui/WorldOfZuulGui/src/main/java/com/mycompany/worldofzuulgui/HjemController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -27,8 +29,6 @@ public class HjemController implements Initializable {
     private Button goFodboldbanen;
     @FXML
     private Pane spawnPane;
-    @FXML
-    private Pane inventoryPane;
     @FXML
     private ImageView inv11;
     @FXML
@@ -54,6 +54,17 @@ public class HjemController implements Initializable {
     private Text scoreTxt;
     @FXML
     private Text sorterTxt;
+    @FXML
+    private ImageView f1;
+    @FXML
+    private ImageView f2;
+    @FXML
+    private ImageView f3;
+
+    private ImageView[] arrayImage;
+
+    int bX = 784;
+    int bY = 164;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -69,80 +80,123 @@ public class HjemController implements Initializable {
                 + "\\src\\main\\java\\com\\mycompany\\JavaBilleder1\\BlaaSkraldespand.png");
         metal_glas_plast.setImage(new Image(metalGlasPlast.getPath()));
 
-        metal_glas_plast.setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent t) {
-                metal_glas_plast.setEffect(hover);
-            }
-        });
+        arrayImage = new ImageView[]{f1, f2, f3};
 
-        metal_glas_plast.setOnMouseExited(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent t) {
-                metal_glas_plast.setEffect(null);
-            }
-        });
+        WorldOfZuul.game.inventory.reloadInv(inv11, inv22);
 
-        File papPair = new File("file:///" + System.getProperty("user.dir")
-                + "\\src\\main\\java\\com\\mycompany\\JavaBilleder1\\RoedSkraldespand.png");
-        papir_pap.setImage(new Image(papPair.getPath()));
+        Room currentRoom = WorldOfZuul.game.getCurrentRoom();
+        currentRoom.trash.clear();
+        currentRoom.spawnTrash(WorldOfZuul.game.inventory);
 
-        papir_pap.setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent t) {
-                papir_pap.setEffect(hover);
-            }
-        });
+        Object[] values = currentRoom.trash.values().toArray();
+        for (int i = 0; i < currentRoom.trash.size(); i++) {
+            Trash currentTrash = (Trash) currentRoom.trash.values().toArray()[i];
 
-        papir_pap.setOnMouseExited(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent t) {
-                papir_pap.setEffect(null);
-            }
-        });
+            arrayImage[i].setId(currentTrash.getTrashType().toString());
 
-        File madSkraldespand = new File("file:///" + System.getProperty("user.dir")
-                + "\\src\\main\\java\\com\\mycompany\\JavaBilleder1\\GulSkraldespand.png");
-        madaffald.setImage(new Image(madSkraldespand.getPath()));
+            File currentImage = new File("file:///" + System.getProperty("user.dir")
+                    + "\\src\\main\\java\\com\\mycompany\\JavaBilleder1\\" + currentTrash.toString() + ".png");
 
-        madaffald.setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent t) {
-                madaffald.setEffect(hover);
-            }
-        });
+            arrayImage[i].setImage(new Image(currentImage.getPath()));
 
-        madaffald.setOnMouseExited(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent t) {
-                madaffald.setEffect(null);
-            }
-        });
+            arrayImage[i].setLayoutX(new Random().nextInt(bX));
+            arrayImage[i].setLayoutY(new Random().nextInt(bY));
 
-        File restSkraldespand = new File("file:///" + System.getProperty("user.dir")
-                + "\\src\\main\\java\\com\\mycompany\\JavaBilleder1\\TyrkisSkraldespand.png");
-        rest_affald.setImage(new Image(restSkraldespand.getPath()));
+            metal_glas_plast.setOnMouseEntered(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+                    metal_glas_plast.setEffect(hover);
+                }
+            });
 
-        rest_affald.setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent t) {
-                rest_affald.setEffect(hover);
-            }
-        });
+            metal_glas_plast.setOnMouseExited(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+                    metal_glas_plast.setEffect(null);
+                }
+            });
 
-        rest_affald.setOnMouseExited(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent t) {
-                rest_affald.setEffect(null);
-            }
-        });
+            File papPair = new File("file:///" + System.getProperty("user.dir")
+                    + "\\src\\main\\java\\com\\mycompany\\JavaBilleder1\\RoedSkraldespand.png");
+            papir_pap.setImage(new Image(papPair.getPath()));
 
-        moveTxt.setText("Besøg: " + WorldOfZuul.game.getMoves());
-        scoreTxt.setText("Point: " + WorldOfZuul.game.getScoreCounter().getScore());
-        sorterTxt.setText("");
+            papir_pap.setOnMouseEntered(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+                    papir_pap.setEffect(hover);
+                }
+            });
+
+            papir_pap.setOnMouseExited(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+                    papir_pap.setEffect(null);
+                }
+            });
+
+            File madSkraldespand = new File("file:///" + System.getProperty("user.dir")
+                    + "\\src\\main\\java\\com\\mycompany\\JavaBilleder1\\GulSkraldespand.png");
+            madaffald.setImage(new Image(madSkraldespand.getPath()));
+
+            madaffald.setOnMouseEntered(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+                    madaffald.setEffect(hover);
+                }
+            });
+
+            madaffald.setOnMouseExited(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+                    madaffald.setEffect(null);
+                }
+            });
+
+            File restSkraldespand = new File("file:///" + System.getProperty("user.dir")
+                    + "\\src\\main\\java\\com\\mycompany\\JavaBilleder1\\TyrkisSkraldespand.png");
+            rest_affald.setImage(new Image(restSkraldespand.getPath()));
+
+            rest_affald.setOnMouseEntered(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+                    rest_affald.setEffect(hover);
+                }
+            });
+
+            rest_affald.setOnMouseExited(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+                    rest_affald.setEffect(null);
+                }
+            });
+
+            File pantPose = new File("file:///" + System.getProperty("user.dir")
+                    + "\\src\\main\\java\\com\\mycompany\\JavaBilleder1\\PantPose.png");
+            pant.setImage(new Image(pantPose.getPath()));
+
+            pant.setOnMouseEntered(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+                    pant.setEffect(hover);
+                }
+            });
+            pant.setOnMouseExited(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+                    pant.setEffect(null);
+                }
+            });
+
+            moveTxt.setText("Besøg: " + WorldOfZuul.game.getMoves());
+            scoreTxt.setText("Point: " + WorldOfZuul.game.getScoreCounter().getScore());
+            sorterTxt.setText("");
+
+        }
     }
 
-    @FXML
+    ;
+
+        @FXML
     private void goParkenAction() throws IOException {
         WorldOfZuul.game.play(new Command(CommandWord.GO, "parken", "", ""));
         App.setRoot("Parken");
@@ -167,42 +221,58 @@ public class HjemController implements Initializable {
     }
 
     @FXML
-    private void invClicked1(MouseEvent event) {
+    private void invClicked1(MouseEvent event
+    ) {
         WorldOfZuul.game.inventory.slotSelectedHandler(event);
     }
 
     @FXML
-    private void invClicked2(MouseEvent event) {
+    private void invClicked2(MouseEvent event
+    ) {
         WorldOfZuul.game.inventory.slotSelectedHandler(event);
     }
 
     @FXML
-    private void metalGlasPlastClicked(MouseEvent event) {
+    private void metalGlasPlastClicked(MouseEvent event
+    ) {
         WorldOfZuul.game.recycleTrash(event, sorterTxt, inv11, inv22);
         scoreTxt.setText("Point: " + WorldOfZuul.game.getScoreCounter().getScore());
     }
 
     @FXML
-    private void papPapirClicked(MouseEvent event) {
+    private void papPapirClicked(MouseEvent event
+    ) {
         WorldOfZuul.game.recycleTrash(event, sorterTxt, inv11, inv22);
         scoreTxt.setText("Point: " + WorldOfZuul.game.getScoreCounter().getScore());
     }
 
     @FXML
-    private void madClicked(MouseEvent event) {
+    private void madClicked(MouseEvent event
+    ) {
         WorldOfZuul.game.recycleTrash(event, sorterTxt, inv11, inv22);
         scoreTxt.setText("Point: " + WorldOfZuul.game.getScoreCounter().getScore());
     }
 
     @FXML
-    private void restClicked(MouseEvent event) {
+    private void restClicked(MouseEvent event
+    ) {
         WorldOfZuul.game.recycleTrash(event, sorterTxt, inv11, inv22);
         scoreTxt.setText("Point: " + WorldOfZuul.game.getScoreCounter().getScore());
     }
 
     @FXML
-    private void pantClicked(MouseEvent event) {
+    private void pantClicked(MouseEvent event
+    ) {
         WorldOfZuul.game.recycleTrash(event, sorterTxt, inv11, inv22);
         scoreTxt.setText("Point: " + WorldOfZuul.game.getScoreCounter().getScore());
+    }
+
+    @FXML
+    private void trashClicked(MouseEvent event) {
+        Room currentRoom = WorldOfZuul.game.getCurrentRoom();
+
+        ArrayList<Trash> list = new ArrayList<>(currentRoom.trash.values());
+
+        WorldOfZuul.game.inventory.trashClicked(event, list, inv11, inv22);
     }
 }
